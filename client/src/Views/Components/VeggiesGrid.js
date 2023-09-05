9fb0ac3ed6b39df5514796f3b8c021b132594c50
immatte <matte_isabelle@yahoo.fr>
2023-09-05 18:03:05 +0200

    Update Veggie_Grid: HandleChangeView

--- client/src/Views/Components/VeggiesGrid.js
+++ client/src/Views/Components/VeggiesGrid.js
@@ -6,6 +6,11 @@ import { Routes, Route, Link } from "react-router-dom";
 /* CHILD OF USERVIEW */
 
 function VeggiesGrid(props) {
+
+    async function handleChangeView (event){
+        console.log(event.target.alt)
+    }
+
     return (
     <div>
         <div className='monthVeggies'>
@@ -14,9 +19,10 @@ function VeggiesGrid(props) {
             props.monthVeggies.map((veggie) => (
                 <li id='veggiesbox'>
                     <img className='veggieImage'
+                         onClick={handleChangeView}
                          src= {veggie.veggie_url}
                          alt= {veggie.veggie_name}
-                    />
+                         />
                     <h5 id='veggieText'> {veggie.veggie_name} </h5>
                 </li>
             )) 
