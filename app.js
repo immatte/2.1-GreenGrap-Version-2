95e755f20ab6f94dac11c9ba43d7f5a51aaee9aa
immatte <matte_isabelle@yahoo.fr>
2023-09-05 17:23:33 +0200

    Update database veggies

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const cors = require('cors'); // add at the top
-app.use(cors()); // add after 'app' is created
 
 var express = require('express');
 var path = require('path');
@@ -7,9 +6,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var veggiesRouter = require('./routes/veggies'); //changed
+// var usersRouter = require('./routes/users');
+var fruitsRouter = require('./routes/fruits'); //added
 
 var app = express();
+app.use(cors()); // add after 'app' is created
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,7 +19,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
+// app.get("/", function(req, res, next) {
+//     res.send("Access the API at path /api");
+//   });
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
+app.use('/veggies', veggiesRouter); //added
+app.use('/fruits', fruitsRouter); //added
 
 module.exports = app;
