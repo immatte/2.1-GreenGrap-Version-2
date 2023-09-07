89099b1ce5a11ef3f6f0fb6e560066e4a2311b1a
immatte <matte_isabelle@yahoo.fr>
2023-09-07 17:53:18 +0200

    Started with header icons, encountered some issues with bootsrap icons

--- client/src/Views/Components/Header.js
+++ client/src/Views/Components/Header.js
@@ -12,13 +12,15 @@ function Header(props) {
 
 
   return (
-         <div className="d-flex flex-nowrap justify-content-between m-0" id="containerApp">
-        <p className="h4" id="h2Subtitle">
-          Vegetables for your season
-        </p>
-        <div>
-          <SelectCountry setSelectedCountry = {props.setSelectedCountry} selectedCountry= {props.selectedCountry}/>
-        </div>
+        <div className="d-flex flex-nowrap justify-content-between m-0" id="containerApp">
+          <i className="bi bi-arrow-down-right-circle-fill"></i>
+          <i className="bi bi-1-circle-fill"></i>
+          <p className="h4" id="h2Subtitle">
+            Vegetables for your season
+          </p>
+          <div>
+            <SelectCountry setSelectedCountry = {props.setSelectedCountry} selectedCountry= {props.selectedCountry}/>
+          </div>
         </div>
   );
 }
